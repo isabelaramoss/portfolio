c79f8122f9101c3aa60f448fca6e46a74b3d9401
Isabela Ramos <isamrteixeira@gmail.com>
2024-08-15 01:34:56 -0300

    ajustando menu responsivo e toggle

--- script.js
+++ script.js
@@ -3,36 +3,35 @@ let menu = document.querySelector('#nav-toggle');
 let navlist = document.querySelector('.nav__list');
 
 menu.onclick = () => {
- menu.classList.toggle('ri-close-large-line');
- navlist.classList.toggle('active');
+  menu.classList.toggle('ri-close-large-line');
+  navlist.classList.toggle('active');
 }
 
 window.onscroll = () => {
- menu.classList.remove('ri-close-large-line');
- navlist.classList.remove('active');
+  menu.classList.remove('ri-close-large-line');
+  navlist.classList.remove('active');
 }
-
 
 //EMAIL JS
 const contactForm = document.getElementById('contact-form'), 
- contactMessage = document.getElementById('contact-message')
+  contactMessage = document.getElementById('contact-message')
 
 const sendEmail = (e) => {
- e.preventDefault()
+  e.preventDefault()
 
- emailjs
-  .sendForm('service_qu2bhu7','template_au5haka','#contact-form','gmiSNWZvOItz-pdlT')
-  .then(() => {
+  emailjs
+    .sendForm('service_qu2bhu7','template_au5haka','#contact-form','gmiSNWZvOItz-pdlT')
+    .then(() => {
    //show sent message
-   contactMessage.textContent = 'Mensagem enviada com sucesso! :)'
+    contactMessage.textContent = 'Mensagem enviada com sucesso! :)'
 
    //remove message after five seconds
-   setTimeout(() =>{
-    contactMessage.textContent = ''
-   }, 5000)
+    setTimeout(() =>{
+      contactMessage.textContent = ''
+    }, 5000)
 
    //clear input fields
-   contactForm.reset()
+    contactForm.reset()
   })
 }
 
